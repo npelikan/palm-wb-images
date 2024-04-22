90d483e6ae0126f2f39807b4909f1e7fcbac6341
Nick Pelikan <nick.pelikan@posit.co>
2024-04-22 16:30:10 -0600

    :angry: bash

--- product-base-pro/install_r.sh
+++ product-base-pro/install_r.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 IFS=' '
-read -ra r_version_arr <<< "$1"
-for R_VERSION in "${r_version_arr[@]}"
+read -ra version_arr <<< "$1"
+for R_VERSION in version_arr
 do
     curl -O https://cdn.rstudio.com/r/ubuntu-2204/pkgs/r-${R_VERSION}_1_amd64.deb
     apt-get install -yq --no-install-recommends ./r-${R_VERSION}_1_amd64.deb
