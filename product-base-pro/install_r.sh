63828199260c2716859773f1e8794af972183fc3
Nick Pelikan <nick.pelikan@posit.co>
2024-04-22 16:27:04 -0600

    tpyo

--- product-base-pro/install_r.sh
+++ product-base-pro/install_r.sh
@@ -5,7 +5,7 @@ read -ra r_version_arr <<< "$1"
 for R_VERSION in "${r_version_arr[@]}"
 do
     curl -O https://cdn.rstudio.com/r/ubuntu-2204/pkgs/r-${R_VERSION}_1_amd64.deb
-    apt-get install -yq --no-install-recommends ./r-${R_VERSON}_1_amd64.deb
+    apt-get install -yq --no-install-recommends ./r-${R_VERSION}_1_amd64.deb
     rm -f ./r-${R_VERSION}_1_amd64.deb
     eval "/opt/R/${R_VERSION}/bin/R" -e 'install.packages("odbc", repos="https://packagemanager.rstudio.com/cran/__linux__/jammy/latest")'
     echo "Installed R v${R_VERSION}"
