be62faf808c49884327b93d6eef957e23054664f
Nick Pelikan <nick.pelikan@posit.co>
2024-04-22 16:31:18 -0600

    its 4:30, take my keyboard away

--- product-base-pro/install_py.sh
+++ product-base-pro/install_py.sh
@@ -2,7 +2,7 @@
 
 IFS=' '
 read -ra version_arr <<< "$1"
-for PYTHON_VERSION in version_arr
+for PYTHON_VERSION in $version_arr
 do
     curl -O https://cdn.rstudio.com/python/ubuntu-2204/pkgs/python-${PYTHON_VERSION}_1_amd64.deb
     apt-get install -yq --no-install-recommends ./python-${PYTHON_VERSION}_1_amd64.deb
--- product-base-pro/install_r.sh
+++ product-base-pro/install_r.sh
@@ -2,7 +2,7 @@
 
 IFS=' '
 read -ra version_arr <<< "$1"
-for R_VERSION in version_arr
+for R_VERSION in $version_arr
 do
     curl -O https://cdn.rstudio.com/r/ubuntu-2204/pkgs/r-${R_VERSION}_1_amd64.deb
     apt-get install -yq --no-install-recommends ./r-${R_VERSION}_1_amd64.deb
